1b10592a8cfbb3de8ad6e973e9464116b7b5e714
Omarep <omar.petricevic@gmail.com>
2023-12-06 12:16:48 +0100

    Added LoginScreen

--- src/LoginScreenForm.java
+++ src/LoginScreenForm.java
@@ -1,2 +1,41 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class LoginScreenForm {
+    public JPanel panel1;
+    private JLabel passwordLabel;
+    private JLabel usernameLabel;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JLabel welcomeLabel; // Changed from welcomeField to welcomeLabel for clarity
+    private JButton loginButton;
+
+    public LoginScreenForm() {
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                // Check if the username and password are correct
+                if ("admin".equals(username) && "123".equals(password)) {
+                    // If login is successful, show the next part of your application
+                    welcomeLabel.setText("Dobrodošli!"); // Set welcome message
+                    // You can here call a method that will change the view to the next part of the application
+                } else {
+                    // If login is unsuccessful, show an option to try again or exit
+                    int action = JOptionPane.showConfirmDialog(panel1, "Nepostojeći korisnik. Pokušajte ponovo?", "Greška pri prijavi", JOptionPane.YES_NO_OPTION);
+                    if (action == JOptionPane.YES_OPTION) {
+                        // User chose to try again, clear the fields for new input
+                        usernameField.setText("");
+                        passwordField.setText("");
+                    } else {
+                        // User chose to exit
+                        System.exit(0);
+                    }
+                }
+            }
+        });
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Use the event dispatch thread to build the UI for thread-safety.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                // Create the frame that will contain our JPanel
+                JFrame frame = new JFrame("LoginScreen");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+                // Create an instance of the LoginScreenForm.
+                LoginScreenForm loginScreenForm = new LoginScreenForm();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+                // Add the panel from LoginScreenForm to the frame.
+                frame.setContentPane(loginScreenForm.panel1);
+
+                // Set the size of the frame based on the contents.
+                frame.pack();
+
+                // Center the frame on the screen.
+                frame.setLocationRelativeTo(null);
+
+                // Make the frame visible.
+                frame.setVisible(true);
+            }
+        });
     }
 }
