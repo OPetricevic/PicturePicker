3559a0ad4e9eface6e69a8829077b2476f66c389
Omarep <omar.petricevic@gmail.com>
2023-12-09 09:55:06 +0100

    Experimenting with buttons

--- src/PicturePickerForm.java
+++ src/PicturePickerForm.java
@@ -2,5 +2,14 @@ import javax.swing.*;
 
 public class PicturePickerForm {
     private JPanel picturePickerField;
-    private JButton pictureOne;
+    private JButton button1;
+    private JButton button3;
+    private JButton button2;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
 }
